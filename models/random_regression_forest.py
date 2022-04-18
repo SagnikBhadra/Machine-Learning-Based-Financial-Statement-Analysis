d7c3f1597b13bc28d4fcf7b3f05dfe59ed0a2c5b
Sagnik Bhadra <36214091+SagnikBhadra@users.noreply.github.com>
2022-04-17 20:34:16 -0700

    Initial Implementation of Random Forest

---
+++ models/random_regression_forest.py
@@ -0,0 +1,18 @@
+from sklearn.ensemble import RandomForestRegressor
+
+class RandomForest:
+    def __init__(self):
+        #CART tree
+        #num_tress_in_forest = 200
+        #Consider depth of tree hyperparameter
+
+        self.n_estimators = 200
+        self.random_forest = RandomForestRegressor(n_estimators = self.n_estimators)
+
+    def train(self, x_train, y_train):
+        self.random_forest_fitted = self.random_forest(x_train, y_train)
+
+    def test(self, x_test):
+        y_predictions = self.random_forest_fitted.predict(x_test)
+        return y_predictions
+        
