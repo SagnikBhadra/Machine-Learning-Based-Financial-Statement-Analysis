e01083bdf2acae80ee721a9de85812efe7604a99
Sagnik Bhadra <36214091+SagnikBhadra@users.noreply.github.com>
2022-04-18 22:09:07 -0700

    Clean part of the data

--- clean_data.py
+++ clean_data.py
@@ -51,9 +51,9 @@ def imputation_softimpute(dataset):
     return SoftImpute().fit_transform(dataset)
 
 def drop_rows_where_SALEQ_ATQ_zero(dataset):
-    filt = (dataset['saleq'] == 0)
+    filt = (dataset['saleq'] != 0)
     dataset = dataset.loc[filt]
-    filt2 = (dataset['atq'] == 0)
+    filt2 = (dataset['atq'] != 0)
     return dataset.loc[filt2]
 
 def exclude_quarters_with_no_accouncement_date(dataset):
@@ -65,3 +65,30 @@ def normalize_data():
     # https://stackoverflow.com/questions/28576540/how-can-i-normalize-the-data-in-a-range-of-columns-in-my-pandas-dataframe
     pass
 
+#dataset = pd.read_csv('data/fundamental_quarterly.csv')
+#dataset = selecting_most_populated_columns(dataset)
+#dataset.to_csv('data/most_populated_columns.csv', index=False)
+
+#dataset = pd.read_csv('data/most_populated_columns.csv')
+#dataset = drop_rows_with_half_missing_values(dataset)
+#dataset.to_csv('data/most_populated_rows_and_columns.csv', index=False)
+
+#dataset = pd.read_csv('data/most_populated_rows_and_columns.csv')
+#dataset = drop_rows_where_SALEQ_ATQ_missing(dataset)
+#dataset.to_csv('data/rows_SALEQ_ATQ_filled.csv', index=False)
+
+#dataset = pd.read_csv('data/rows_SALEQ_ATQ_filled.csv')
+#softimpute_np_arr = imputation_softimpute(dataset)
+#dataset = pd.DataFrame(softimpute_np_arr, columns=['acoq', 'actq', 'ancq', 'aoq', 'apq', 'atq', 'capsq', 'ceqq', 'cheq','csh12q', 'cshoq', 'cshprq', 'cshpry', 'cstkq', 'dlcq', 'dlttq', 'dpactq', 'icaptq', 'invtq', 'lcoq', 'lctq', 'lltq', 'loq', 'lseq', 'ltmibq','ltq', 'mibq', 'mibtq', 'ppegtq', 'ppentq', 'pstknq', 'pstkq','pstkrq', 'rectq', 'req', 'seqq', 'tstkq', 'txditcq', 'txpq', 'wcapq', 'aolochy', 'apalchy', 'aqcy', 'capxy', 'chechy', 'dltisy', 'dltry','dpcy', 'dvy', 'esubcy', 'exrey', 'fiaoy', 'fincfy', 'fopoy', 'ibcy','intpny', 'invchy', 'ivacoy', 'ivchy', 'ivncfy', 'ivstchy', 'oancfy', 'prstkcy', 'recchy', 'sivy', 'sppivy', 'sstky', 'txdcy', 'xidocy', 'acchgq', 'cogsq', 'cogsy', 'cstkeq', 'doq', 'doy', 'dpq', 'dpy', 'dvpq','dvpy', 'epsfiq', 'epsfiy', 'epsfxq', 'epsfxy', 'epspiq', 'epspiy','epspxq', 'epspxy', 'epsx12', 'ibadjq', 'ibadjy', 'ibcomq', 'ibq','iby', 'miiq', 'miiy', 'niq', 'niy', 'nopiq', 'nopiy', 'oiadpq', 'oiadpy','oibdpq', 'opepsq', 'piq', 'piy', 'revtq', 'revty', 'saleq', 'saley', 'spiq','spiy', 'txtq', 'txty', 'xidoq', 'xidoy', 'xintq', 'xiq', 'xiy', 'xoprq','xopry', 'xsgaq'])
+#dataset.to_csv('data/softimpute.csv', index=False)
+#percent_missing = dataset.isnull().sum() * 100 / len(dataset)
+#print(f'Percent Missing: {percent_missing}')
+
+#dataset = pd.read_csv('data/softimpute.csv')
+#dataset = drop_rows_where_SALEQ_ATQ_zero(dataset)
+#dataset.to_csv('data/rows_SALEQ_ATQ_populated.csv', index=False)
+
+#dataset = exclude_quarters_with_no_accouncement_date(dataset)
+#dataset = normalize_data(dataset)
+
+#print(f'Number of rows: {dataset.shape[0]}, Number of columns: {dataset.shape[1]}')
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
 
 from models import DNN, RNN, RandomForest
+from clean_data import selecting_most_populated_columns, drop_rows_with_half_missing_values, drop_rows_where_SALEQ_ATQ_missing, imputation_softimpute, drop_rows_where_SALEQ_ATQ_zero, exclude_quarters_with_no_accouncement_date, normalize_data
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     #Load data
+    dataset = pd.read_csv('data/cleaned_data.csv')
 
     #Set up model
     model = RNN().to(device)
 
     #Initialize loss and optimizer
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
     #Train the model
