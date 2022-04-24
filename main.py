bf06122cd47d19302aa0ae0a6e9f8a60e6da24b7
Sagnik Bhadra <36214091+SagnikBhadra@users.noreply.github.com>
2022-04-24 02:09:13 -0700

    Create separate train and test functions for ML and DL models

--- main.py
+++ main.py
@@ -58,6 +58,55 @@ def accuracy(output, target):
 
     return acc
 
+def ML_train(epoch, data_loader, model):
+    iter_time = AverageMeter()
+    losses = AverageMeter()
+    acc = AverageMeter()
+
+    
+
+    for idx, (data, target) in enumerate(data_loader):
+        start = time.time()
+        if torch.cuda.is_available():
+            data = data.cuda()
+            target = target.cuda()
+
+        model.train(data, target)
+        iter_time.update(time.time() - start)
+
+    if idx % 10 == 0:
+            print(('Epoch: [{0}][{1}/{2}]\t'
+                   'Time {iter_time.val:.3f} ({iter_time.avg:.3f})\t')
+                  .format(epoch, idx, len(data_loader), iter_time=iter_time))
+
+
+def ML_validation(epoch, val_loader, model, criterion):
+    iter_time = AverageMeter()
+    losses = AverageMeter()
+    acc = AverageMeter()
+
+    for idx, (data, target) in enumerate(val_loader):
+        start = time.time()
+
+        out = model.test(data)
+        loss = criterion(out, target)
+
+        batch_acc = accuracy(out, target)
+
+        losses.update(loss, out.shape[0])
+        acc.update(batch_acc, out.shape[0])
+
+        iter_time.update(time.time() - start)
+
+        if idx % 10 == 0:
+            print(('Epoch: [{0}][{1}/{2}]\t'
+                   'Time {iter_time.val:.3f} ({iter_time.avg:.3f})\t'
+                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                   'Prec @1 {top1.val:.4f} ({top1.avg:.4f})\t')
+                  .format(epoch, idx, len(val_loader), iter_time=iter_time, loss=losses, top1=acc))
+
+    
+
 def train(epoch, data_loader, model, optimizer, criterion):
     iter_time = AverageMeter()
     losses = AverageMeter()
@@ -195,12 +244,19 @@ def main():
     best_cm = None
     best_model = None
 
-    for epoch in range(num_epochs):
-        #Train the model
-        train(epoch, data_loader, model, optimizer, criterion)
+    if args.model == "RNN" or args.model == "DNN":
+        for epoch in range(num_epochs):
+            #Train the model
+            train(epoch, data_loader, model, optimizer, criterion)
 
-        #Validate the model
-        acc, cm = validate(epoch, val_loader, model, criterion)
+            #Validate the model
+            acc, cm = validate(epoch, val_loader, model, criterion)
+    elif args.model == "OLS" or args.model == "LASSO" or args.model == "RandomForest":
+        for epoch in range(num_epochs):
+            #Train the model
+            ML_train(epoch, data_loader, model)
+            ML_validation(epoch, val_loader, model, criterion)
+            
 
 
 
