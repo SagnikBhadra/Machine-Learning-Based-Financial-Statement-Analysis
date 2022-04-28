17749685535988bfc32a395aa9ff4050d95ef25a
Corey Elowsky <coreyelowsky@gmail.com>
2022-04-28 01:54:56 -0400

    flexible batches for rnn

--- main_rnn.py
+++ main_rnn.py
@@ -2,47 +2,61 @@ import models.rnn
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+class RNN_Dataset(Dataset):
+
+	def __init__(self, data_path):
+
+		# load data
+		data = np.load(data_path, allow_pickle=True)
+
+		# some pre processing
+		self.tickers = data[:,0]
+		self.x = data[:,2:-2]
+		self.y = data[:,-1]
+
+		# generate sequences
+		self.x_sequences, self.y_sequences = self.generate_sequences()
 
 
-def generate_sequences(x, y, tickers):
 
-	'''
-	generates sequence data for rnn based on ticker
+	def __len__(self):
 
-	'''
-	
-	x_sequences = []
-	y_sequences = []
-	i = 0
-	while i < len(tickers) - 4:
+		return len(self.x_sequences)
+
+	def __getitem__(self, idx):
+
+		return self.x_sequences[idx], self.y_sequences[idx]
+
+	def get_tickers(self):
+
+		return self.tickers
+
+
+	def generate_sequences(self):
+
+		'''
+		generates sequence data for rnn based on ticker
+
+		'''
 		
-		# if next 5 have same ticker then generate sequence
-		if (tickers[i:i+5] == tickers[i]).all():
-			x_seq = torch.unsqueeze(torch.from_numpy(x[i:i+4,:].astype(np.float32)), 0)
-			y_seq = torch.tensor(y[i+4])
+		x_sequences = []
+		y_sequences = []
+		i = 0
+		while i < len(self.tickers) - 4:
+			
+			# if next 5 have same ticker then generate sequence
+			if (self.tickers[i:i+5] == self.tickers[i]).all():
+				x_seq = torch.from_numpy(self.x[i:i+4,:].astype(np.float32))
+				y_seq = torch.tensor(self.y[i+4])
 
-			x_sequences.append(x_seq)
-			y_sequences.append(y_seq)
-		
-		i += 1
+				x_sequences.append(x_seq)
+				y_sequences.append(y_seq)
+			
+			i += 1
 
-	return x_sequences, y_sequences
-
-
-def load_data(path):
-
-	'''
-	loads data
-
-	'''
-
-	data = np.load(path, allow_pickle=True)
-
-	tickers = data[:,0]
-	x_train = data[:,2:-2]
-	y_train = data[:,-1]
-
-	return x_train, y_train, tickers
+		return x_sequences, y_sequences
 
 
 
@@ -50,27 +64,26 @@ def main():
 
 	# learning rate
 	lr = .005
-	num_epochs = 3
-	train_path = './data/rnn_train.npy'
+	num_epochs = 5
+	batch_size = 5
+	train_path = './data/rnn_train_data_test.npy'#'./data/rnn_train.npy'
 	val_path = './data/rnn_val.npy'
 
 	# get device
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 	# instantiate model
-	model = models.rnn.RNN(device).to(device)
+	model = models.rnn.RNN(device=device).to(device)
 
 	# define loss and optimizer
 	criterion = nn.MSELoss()
 	optimizer = torch.optim.Adam(model.parameters(), lr=lr) 
 
-	# load data
-	x_train, y_train, tickers_train = load_data(train_path)
-	x_val, y_val, tickers_val = load_data(val_path)
 
-	# generate sequences
-	x_sequences_train, y_sequences_train = generate_sequences(x_train, y_train, tickers_train)
-	x_sequences_val, y_sequences_val = generate_sequences(x_val, y_val, tickers_val)
+	# create datasets
+	train_dataset = RNN_Dataset(train_path)
+	train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
 
 	# train loop
 	train_losses = []
@@ -78,21 +91,20 @@ def main():
 	for epoch in range(num_epochs):
 
 		print(f'Epoch {epoch}')
-		print('# Batches:', len(x_sequences_train))
+		print('# Batches:', len(train_dataloader))
 
 		train_loss = 0
 
-		for batch, (x_sequence, y_sequence) in enumerate(zip(x_sequences_train, y_sequences_train)):
+		for batch, (x_sequence, y_sequence) in enumerate(train_dataloader):
 
 			if batch and batch % 1000 == 0:
 				print(batch)
 
 			# forward pass
 			out = model.forward(x_sequence)
-
+	
 			# compute loss
 			loss = 	criterion(out, y_sequence)
-
 
 			# zero gradients
 			optimizer.zero_grad()
--- models/rnn.py
+++ models/rnn.py
@@ -20,26 +20,28 @@ class RNN(nn.Module):
         self.input_size = 121
         self.num_layers = 4
         self.hidden_size = 20
-        self.num_classes = 1
+        self.output_size = 1
+
         
         self.rnn = nn.GRU(self.input_size, self.hidden_size, self.num_layers, batch_first=True)
        
-        # x -> (batch_size, sequence_size, input_size)
-        self.linear_layer = nn.Linear(self.hidden_size, self.num_classes)
+        self.linear_layer = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, x):
 
-        # initialize hidden state
 
-        hidden_initial = torch.zeros(self.num_layers, 1,  self.hidden_size).to(self.device)
+        # initialize hidden state
+        hidden_initial = torch.zeros(self.num_layers, x.shape[0],  self.hidden_size).to(self.device)
 
         out, _ = self.rnn(x, hidden_initial)
+
 
         # take last hidden state
         out = out[:, -1, :]
 
         # send through linear layer
         out = self.linear_layer(out)
+  
 
-        return out[0][0]
+        return out.flatten()
 
